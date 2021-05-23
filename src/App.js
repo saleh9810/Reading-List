ee62d8d03ca8d25217f6bd6d7555ef63d8bd906e
iamsaleh <salehfarag161@gmail.com>
2021-05-23 02:49:31 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import BookContextProvider from './contexts/BookContext';
+import NewBookForm from './components/BookForm';
+
+function App() {
+  return (
+    <div className="App">
+     <BookContextProvider>
+      <Navbar />
+      <BookList />
+      <NewBookForm />
+     </BookContextProvider>
+   
+    </div>
+  );
+}
+
+export default App;
+ 
